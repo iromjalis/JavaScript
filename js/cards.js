7d981c2ed20016ac319b0c6f5e21a6fc6edd0a25
Irina Romjalis <iromjalis@gmail.com>
2021-03-07 17:52:31 +0200

    nav + gallery

--- js/cards.js
+++ js/cards.js
@@ -4,6 +4,7 @@ export default [
     url: 'https://a.d-cd.net/8e459a5s-960.jpg',
     id: 1,
     description: 'BMW',
+    price: "1500"
   },
   {
     name: 'Mercedes-benz',
@@ -11,12 +12,16 @@ export default [
       'https://www.mercedes-benz.ua/passengercars/mercedes-benz-cars/models/e-class/coupe-c238/design/lines-comparison/_jcr_content/comparisonslider/par/comparisonslide_3600/exteriorImage.MQ6.12.20190816144830.jpeg',
     id: 2,
     description: 'Mercedes-benz',
+    price: "1000"
+
   },
   {
     name: 'Audi',
     url: 'https://img1.automoto.ua/overview/Audi-A4-2021-912-huge-1813.jpg',
     id: 3,
     description: 'Audi',
+    price: "800"
+
   },
   {
     name: 'Mazda',
@@ -24,6 +29,8 @@ export default [
       'https://kolesa-uploads.ru/-/b6f0a8c4-9963-43bd-94be-686f93bca7d5/mazda-6-front2-mini.jpg',
     id: 4,
     description: 'Mazda',
+    price: "500"
+
   },
   {
     name: 'Ford',
@@ -31,5 +38,7 @@ export default [
       'https://avtomarket.ru/stuff/user_upload/218740/218740_1024x768_m6t07diu7w.jpg',
     id: 5,
     description: 'Ford',
+    price: "400"
+
   },
 ];
--- js/cardsItem.js
+++ js/cardsItem.js
@@ -1,49 +1,72 @@
-import cardsArr from './cards.js';
+import cardsArr from './gallery-items.js';
+const nav =['home', 'about us', 'page', 'shop','blog', 'contact us']
 
-const listEl = document.createElement('ul');
-console.log(listEl);
-listEl.classList.add('cards-list');
+const galleryRef = document.createElement('ul')
+galleryRef.classList.add('gallery')
 
-const modalImgRef = document.querySelector('.modal-img');
-const modalRef = document.querySelector('.modal');
+const bodyRef = document.querySelector('body')
+bodyRef.prepend(galleryRef)
 
-cardsArr.forEach(img => {
-  listEl.insertAdjacentHTML(
-    'beforeend',
-    `<li><img src=${img.url} alt="${img.description}" /></li>`,
-  );
-});
-// const markup = cardsArr
-//   .map(
-//     ({ name, url, id, description }) =>
-//       `<li><p>${name}</p><img src='${url}' alt='${description}' id='${id}' width='320'></img></li>`,
-//   )
-//   .join(' ');
+cardsArr.forEach(item=>{
+  galleryRef.insertAdjacentHTML('beforeend',
+  `<li class="gallery__item" ><img src="${item.preview}" alt="${item.description}" width="250" height="auto" />
+  <h2>${item.description}</h2>
+  <p>${item.price}$</p>
+  <button type="button">Add</button></li> `)
+})
 
-// listEl.insertAdjacentHTML('afterbegin', markup);
+const originalImgRef = document.createElement('img')
+console.log(originalImgRef);  
+originalImgRef.classList.add('original__image')
 
-listEl.addEventListener('click', e => {
-  console.log('hello');
-  if (e.target.localName === 'img') {
-    modalRef.style.display = 'block';
-    modalImgRef.src = e.target.src;
+bodyRef.prepend(originalImgRef)
+
+const galleryItemRef = document.querySelector(".gallery__item")
+const buttonRef = document.querySelectorAll('button')
+
+
+galleryItemRef.addEventListener('click', e=>{
+  console.log(e.target);
+  console.dir(e.target);
+  // console.log(originalImgRef.src);
+
+  if(e.target.localName === 'img'){
+    originalImgRef.src = e.target.src
+    originalImgRef.classList.add('is-open')
   }
-});
-
-modalRef.addEventListener('click', e => {
-  console.log('hello');
-  if (e.target.localName !== 'img') {
-    modalRef.style.display = 'none';
-    modalImgRef.src = '';
+  if(e.target.localName === "button"){
+console.log('hello');
+e.target.textContent = "Added"
+e.target.style.backgroundColor = "green"
+  
   }
-});
+})
 
-window.addEventListener('keydown', e => {
-  console.log(e.keyCode);
-  if (e.key === 'Escape') {
-    modalRef.style.display = 'none';
-    modalImgRef.src = '';
+
+bodyRef.addEventListener('click', e=>{
+  if(e.target.localName !== "img" ){
+    originalImgRef.src = ""
+    originalImgRef.classList.remove('is-open')
+
+  } else if(e.target.localName === 'img'){
+    originalImgRef.src = e.target.src
+    originalImgRef.classList.add('is-open')
   }
-});
+})
+bodyRef.addEventListener('keydown', e=>{
 
-modalRef.addEventListener('click');
+  if(e.key === "Escape"){
+  originalImgRef.src = ""
+  originalImgRef.classList.remove('is-open')
+
+ }
+})
+
+//________________________________
+const menuRef=document.createElement('nav')
+menuRef.classList.add('nav')
+bodyRef.prepend(menuRef)
+
+nav.forEach(item => {
+  menuRef.insertAdjacentHTML("beforeend", `<li class="nav__item"><a href="">${item}</a></li>`)
+})
