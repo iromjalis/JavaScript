6de35b72c2ab2803d6abca1cec7f6e36fa4811e4
Irina Romjalis <iromjalis@gmail.com>
2021-03-11 19:16:38 +0200

    repeta module 8

--- js/test.js
+++ js/test.js
@@ -1,33 +1,77 @@
-const nav = document.querySelector(".js-nav");
+const navRef = document.querySelector('.nav')
+const clickedBtn = new Set()
 
-nav.addEventListener('click', handleNavClick)
-
-
-function handleNavClick(e){
-e.preventDefault()
-
-const target = e.target
-
-if(target.nodeName !== 'A'){
-    return
-``
-}
-setActiveLink(target)
-
-}
-
-function setActiveLink(nextActiveLink){
-    const currentActiveLink = nav.querySelector('a.active')
-
-    if(currentActiveLink){
-        currentActiveLink.classList.remove('active')
+navRef.addEventListener('click', e=>{
+    if(e.target.nodeName !== 'A'){
+        return
     }
-    nextActiveLink.classList.add('active')
-}
-window.addEventListener(
-    'scroll',
-    _.debounce(() => {
-      console.log('Scroll event handler invocation every 300ms.');
-    }, 3000),
-  );
+    console.dir(e.target);
+
+
+    // const currentTarget = navRef.querySelector('a.active')
+    // if (currentTarget){
+    //     currentTarget.classList.remove('active')
+    // }
+    // e.target.classList.add('active')
+
+    const isActive = e.target.classList.contains('active')
+
+    if(isActive){
+        clickedBtn.delete(e.target.innerText)
+    }else{
+        clickedBtn.add(e.target.innerText)
+    }
+
+    console.log(clickedBtn);
+})
+
+//________________________________________________
+const colors = [
+    { hex: '#f44336', rgb: '244,67,54' },
+    { hex: '#e91e63', rgb: '233,30,99' },
+    { hex: '#9c27b0', rgb: '156,39,176' },
+    { hex: '#673ab7', rgb: '103,58,183' },
+    { hex: '#3f51b5', rgb: '63,81,181' },
+    { hex: '#2196f3', rgb: '33,150,243' },
+    { hex: '#00bcd4', rgb: '0,188,212' },
+    { hex: '#009688', rgb: '0,150,136' },
+    { hex: '#4caf50', rgb: '76,175,80' },
+    { hex: '#ffeb3b', rgb: '255,235,59' },
+    { hex: '#ff9800', rgb: '255,152,0' },
+    { hex: '#795548', rgb: '121,85,72' },
+    { hex: '#607d8b', rgb: '96,125,139' },
+  ];
+
   
+  const paletteContainer = document.querySelector('.js-palette')
+
+  const markup = createColorCardsMarkup(colors)
+
+  function createColorCardsMarkup(colors) {
+    return colors
+      .map(({ hex, rgb }) => {
+        return `
+      <div class="color-card">
+       <div><div><div> <div
+       class="color-swatch"
+       data-hex="${hex}"
+       data-rgb="${rgb}"
+       style="background-color: ${hex}"
+     ></div></div></div></div>
+        <div class="color-meta">
+          <p>HEX: ${hex}</p>
+          <p>RGB: ${rgb}</p>
+        </div>
+      </div>
+      `;
+      })
+      .join('');
+  }
+  
+paletteContainer.insertAdjacentHTML('beforeend', markup)
+const body = document.querySelector('body')
+
+paletteContainer.addEventListener('click', (e)=>{
+  body.style.backgroundColor = e.target.dataset.hex
+  
+})
--- js/toDoList.js
+++ js/toDoList.js
@@ -1,61 +1,92 @@
-const formRef = document.querySelector('.form');
-const inputRef = document.querySelector('.input');
-const filterRef = document.querySelector('.filter');
-const btnRef = document.querySelector('.btn');
-const clearRef = document.querySelector('.clear');
-const listRef = document.querySelector('.list');
-const arr = [];
-formRef.addEventListener('submit', e => {
-  e.preventDefault();
-  arr.push(inputRef.value);
-  listRef.innerHTML = '';
-  const markup = arr.map(item => `<li><p>${item}</p></li>`).join(' ');
-  listRef.insertAdjacentHTML('beforeend', markup);
-  inputRef.value = '';
-  inputRef.focus();
+const tech = [
+  { label: 'HTML' },
+  { label: 'CSS' },
+  { label: 'JavaScript' },
+  { label: 'Node.js' },
+  { label: 'React' },
+  { label: 'Vue' },
+  { label: 'Next.js' },
+  { label: 'Mobx' },
+  { label: 'Redux' },
+  { label: 'React Router' },
+  { label: 'GraphQl' },
+  { label: 'PostgreSQL' },
+  { label: 'MongoDB' },
+];
+
+/*
+ * 1. Рендерим разметку элементов списка
+ * 2. Слушаем изменение фильтра
+ * 3. Фильтруем данные и рендерим новые элементы
+ */
+
+const refs = {
+  list: document.querySelector('.js-list'),
+  input: document.querySelector('#filter'),
+};
+
+refs.input.addEventListener('input', (onFilterChange));
+
+const listItemsMarkup = createListItemsMarkup(tech);
+renderHTML(listItemsMarkup)
+
+function createListItemsMarkup(items) {
+  return items.map(item => `<li>${item.label}</li>`).join('');
+}
+
+function onFilterChange(e){
+  console.log(e.target.value);
+  const filter = e.target.value.toLowerCase()
+
+  const filteredEl = tech.filter(t=> t.label.toLowerCase().includes(filter))
+  
+  const listItemsMarkup = createListItemsMarkup(filteredEl)
+  // console.log(listItemsMarkup);
+
+  renderHTML(listItemsMarkup)
+}
+
+
+function renderHTML (markup){
+  refs.list.innerHTML = markup
+}
+
+//-------------------------------------------
+
+if ('loading' in HTMLImageElement.prototype) {
+  console.log('Браузер поддерживает lazyload');
+  addSrcAttrToLazyImages();
+} else {
+  console.log('Браузер НЕ поддерживает lazyload');
+  addLazySizesScript();
+}
+
+const lazyImages = document.querySelectorAll('img[data-src]');
+
+lazyImages.forEach(image => {
+  image.addEventListener('load', onImageLoaded, { once: true });
 });
-filterRef.addEventListener('input', e => {
-  const filteredArr = arr.filter(item => item.includes(e.currentTarget.value));
-  const markup = filteredArr.map(item => `<li><p>${item}</p></li>`).join(' ');
-  if (filteredArr.length > 0) {
-    listRef.innerHTML = '';
-    listRef.insertAdjacentHTML('beforeend', markup);
-  } else {
-    listRef.innerHTML = '';
-  }
-});
-clearRef.addEventListener('click', () => {
-  arr = [];
-});
 
-//______________________________________________
+function onImageLoaded(evt) {
+  console.log('Картинка загрузилась');
+  evt.target.classList.add('appear');
+}
 
-// const formRef = document.querySelector('form');
-// const inputRef = document.querySelector('.input');
-// const filterRef = document.querySelector('.filter');
-// const btnRef = document.querySelector('.btn');
-// const listRef = document.querySelector('.list');
-// const arr = [];
-// // console.log(inputRef.value);
+function addLazySizesScript() {
+  const script = document.createElement('script');
+  script.src =
+    'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js';
+  script.integrity =
+    'sha512-TmDwFLhg3UA4ZG0Eb4MIyT1O1Mb+Oww5kFG0uHqXsdbyZz9DcvYQhKpGgNkamAI6h2lGGZq2X8ftOJvF/XjTUg==';
+  script.crossOrigin = 'anonymous';
 
-// formRef.addEventListener('submit', e => {
-//   e.preventDefault();
+  document.body.appendChild(script);
+}
 
-//   arr.push(inputRef.value);
-//   listRef.innerHTML = '';
-//   const markup = arr.map(item => `<li><p>${item}</p></li>`).join(' ');
-//   listRef.insertAdjacentHTML('beforeend', markup);
-//   inputRef.value = '';
-//   inputRef.focus();
-// });
+function addSrcAttrToLazyImages() {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
 
-// filterRef.addEventListener('input', e => {
-//   const filteredArr = arr.filter(item => item.includes(e.currentTarget.value));
-//   const markup = filteredArr.map(item => `<li><p>${item}</p></li>`).join(' ');
-//   if (filteredArr.length > 0) {
-//     listRef.innerHTML = '';
-//     listRef.insertAdjacentHTML('beforeend', markup);
-//   } else {
-//     listRef.innerHTML = '';
-//   }
-// });
+  lazyImages.forEach(img => {
+    img.src = img.dataset.src;
+  });
+}
